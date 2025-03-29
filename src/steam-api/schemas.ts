5085dbfcdb326a7f089516d200c961c9ffb1407d
suzuki3jp <suzuki3.info@gmail.com>
2025-03-29 09:34:00 +0900

    refactor: remove unused code

--- src/steam-api/schemas.ts
+++ src/steam-api/schemas.ts
@@ -1,59 +1,5 @@
 import { z } from "zod";
 
-const hoge = {
-    response: {
-        players: [
-            {
-                steamid: "76561199481414497",
-                communityvisibilitystate: 3,
-                profilestate: 1,
-                personaname: "P1XEL",
-                profileurl:
-                    "https://steamcommunity.com/profiles/76561199481414497/",
-                avatar: "https://avatars.steamstatic.com/0e96fd1da4c91017a7c1de980d6361b139e6831d.jpg",
-                avatarmedium:
-                    "https://avatars.steamstatic.com/0e96fd1da4c91017a7c1de980d6361b139e6831d_medium.jpg",
-                avatarfull:
-                    "https://avatars.steamstatic.com/0e96fd1da4c91017a7c1de980d6361b139e6831d_full.jpg",
-                avatarhash: "0e96fd1da4c91017a7c1de980d6361b139e6831d",
-                personastate: 4,
-                realname: "??????",
-                primaryclanid: "103582791429521408",
-                timecreated: 1677182817,
-                personastateflags: 0,
-            },
-        ],
-    },
-};
-
-const huge = {
-    response: {
-        players: [
-            {
-                steamid: "76561199481414496",
-                communityvisibilitystate: 3,
-                profilestate: 1,
-                personaname: "suzuki",
-                commentpermission: 1,
-                profileurl:
-                    "https://steamcommunity.com/profiles/76561199481414496/",
-                avatar: "https://avatars.steamstatic.com/1893d7c151ac71f1182e4097df5072dbfd996d6c.jpg",
-                avatarmedium:
-                    "https://avatars.steamstatic.com/1893d7c151ac71f1182e4097df5072dbfd996d6c_medium.jpg",
-                avatarfull:
-                    "https://avatars.steamstatic.com/1893d7c151ac71f1182e4097df5072dbfd996d6c_full.jpg",
-                avatarhash: "1893d7c151ac71f1182e4097df5072dbfd996d6c",
-                lastlogoff: 1742646948,
-                personastate: 1,
-                primaryclanid: "103582791429521408",
-                timecreated: 1677061691,
-                personastateflags: 0,
-                loccountrycode: "JP",
-            },
-        ],
-    },
-};
-
 /**
  * Docs: https://developer.valvesoftware.com/wiki/Steam_Web_API#GetPlayerSummaries_(v0002)
  */
